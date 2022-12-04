92efa1cb3cca189c4f78ed6787a2ddcbe1a3b8c2
HUSEYIN6134 <huseyin.murat@st.uskudar.edu.tr>
2022-12-04 13:47:37 +0300

    RecapDemo_Classes
    Engin Demiroğ Java Kursu

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dörtişlem = new DortIslem();
+		int sonuc = dörtişlem.Topla(5, 9);
+		System.out.println("Sonuç: " + sonuc);
+		
+
+	}
+
+}
